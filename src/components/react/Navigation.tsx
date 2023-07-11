1a12d294586156c8d61aa6caec376253923c0cc5
John William Davis <jwdavisdeveloper@gmail.com>
2023-07-11 16:35:40 -0700

    begin work on navigation and links of react nav

--- src/components/react/Navigation.tsx
+++ src/components/react/Navigation.tsx
@@ -2,16 +2,26 @@ import React from 'react';
 
 export default function Navigation() {
   return (
-    <div className='bg-yellow-200 flex items-baseline justify-between '>
+    <div className='bg-yellow-200 flex items-baseline justify-between p-4'>
       <div>
-        <span className='text-orange-700 '>Icon Here</span>
+        <a href='/'>
+          <span className='text-orange-200 hidden md:block p-2 bg-orange-400'>Icon Here</span>
+        </a>
       </div>
       <nav>
-        <ul className='flex justify-between text-orange-600'>
-          <li>Bio</li>
-          <li>Painting</li>
-          <li>Poetry</li>
-          <li>Contact</li>
+        <ul className='flex justify-between text-orange-600 gap-5'>
+          <li>
+            <a href='/bio'>bio</a>
+          </li>
+          <li>
+            <a href='/painting'>painting</a>
+          </li>
+          <li>
+            <a href='/poetry'>poetry</a>
+          </li>
+          <li>
+            <a href='/contact'>contact</a>
+          </li>
         </ul>
       </nav>
     </div>
