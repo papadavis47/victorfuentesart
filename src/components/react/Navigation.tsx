93f6fe3bac582c134a1077e67902182c5552d586
John William Davis <jwdavisdeveloper@gmail.com>
2023-07-12 01:19:06 -0700

    work on slot for react component - create a nav astro component

--- src/components/react/Navigation.tsx
+++ src/components/react/Navigation.tsx
@@ -1,29 +1,14 @@
 import React from 'react';
 
-export default function Navigation() {
+export default function Navigation({ links }: { links: JSX.Element }) {
   return (
-    <div className='bg-yellow-200 flex items-baseline justify-between p-4'>
+    <div className='bg-yellow-700 flex items-baseline justify-between p-4'>
       <div>
         <a href='/'>
-          <span className='text-orange-200 hidden md:block p-2 bg-orange-400'>Icon Here</span>
+          <span className='text-white hidden md:block p-2 bg-orange-400'>Icon Here</span>
         </a>
       </div>
-      <nav>
-        <ul className='flex justify-between text-orange-600 gap-5'>
-          <li>
-            <a href='/bio'>bio</a>
-          </li>
-          <li>
-            <a href='/painting'>painting</a>
-          </li>
-          <li>
-            <a href='/poetry'>poetry</a>
-          </li>
-          <li>
-            <a href='/contact'>contact</a>
-          </li>
-        </ul>
-      </nav>
+      {links}
     </div>
   );
 }
