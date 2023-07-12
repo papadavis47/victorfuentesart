9870eedc3f79b7b0e49224e74b73686e1181e695
John William Davis <jwdavisdeveloper@gmail.com>
2023-07-12 15:56:52 -0700

    working with images and initial copy - with overlays

--- src/components/react/Navigation.tsx
+++ src/components/react/Navigation.tsx
@@ -1,9 +1,9 @@
 export default function Navigation({ children }: { children: any }) {
   return (
-    <div className='bg-yellow-700 flex items-baseline justify-between p-4'>
+    <div className='bg-gradient-to-l from-red-600 to-slate-500 flex items-baseline justify-between p-4'>
       <div>
         <a href='/'>
-          <span className='text-white  p-2 bg-orange-400'>Icon Here</span>
+          <span className='text-white  p-2 bg-slate-800 rounded'>Icon Here</span>
         </a>
       </div>
       <div className='hidden md:block'>{children}</div>
