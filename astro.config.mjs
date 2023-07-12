f311d7610ee6463375689e275f3e19419131e40b
John William Davis <jwdavisdeveloper@gmail.com>
2023-07-12 00:12:56 -0700

    add contact form and image in assets folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  experimental: {
+    assets: true,
+  },
+  integrations: [react(), tailwind()],
 });
