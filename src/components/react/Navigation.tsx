3f118886929f435916fc4620b87edfbb69edffe0
John William Davis <jwdavisdeveloper@gmail.com>
2023-07-12 01:28:56 -0700

    update README and work on Navigation component responsiveness

--- src/components/react/Navigation.tsx
+++ src/components/react/Navigation.tsx
@@ -5,10 +5,10 @@ export default function Navigation({ links }: { links: JSX.Element }) {
     <div className='bg-yellow-700 flex items-baseline justify-between p-4'>
       <div>
         <a href='/'>
-          <span className='text-white hidden md:block p-2 bg-orange-400'>Icon Here</span>
+          <span className='text-white  p-2 bg-orange-400'>Icon Here</span>
         </a>
       </div>
-      {links}
+      <div className='hidden md:block'>{links}</div>
     </div>
   );
 }
