394cf2643820c3655afb256939b83e37d60ce458
John William Davis <jwdavisdeveloper@gmail.com>
2023-09-06 21:54:41 -0700

    work on poem component: styling, and add data from actual poems

--- src/components/react/Poem.tsx
+++ src/components/react/Poem.tsx
@@ -16,13 +16,22 @@ export default function Poem({ spanishTitle, englishTitle, data }: Props) {
   const [english, setEnglish] = useState(false);
   return (
     <>
-      <h1>{english ? englishTitle : spanishTitle}</h1>
+      <h1 className='text-3xl font-bold md:text-4xl mb-4'>
+        {english ? englishTitle : spanishTitle}
+      </h1>
       <div>
         {data.map((item) => (
-          <p key={item.id}>{english ? item.englishLine : item.spanishLine}</p>
+          <p key={item.id} className='py-1 md:text-lg text-md font-semibold'>
+            {english ? item.englishLine : item.spanishLine}
+          </p>
         ))}
       </div>
-      <button onClick={() => setEnglish(!english)}>{english ? 'Spanish' : 'English'}</button>
+      <button
+        className=' mt-6 block w-full sm:w-auto rounded-md bg-slate-600 px-3.5 py-2.5 text-center text-base font-semibold text-white shadow-sm hover:bg-slate-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-600'
+        onClick={() => setEnglish(!english)}
+      >
+        {english ? 'read spanish version' : 'leer la versión en inglés'}
+      </button>
     </>
   );
 }
--- src/data/poems.ts
+++ src/data/poems.ts
@@ -1,19 +1,49 @@
 export const poems = {
-  testPoem: [
+  imagenesDeAmor: [
     {
       id: 1,
-      englishLine: 'I am a test poem',
-      spanishLine: 'Soy un poema de prueba',
+      englishLine: '1',
+      spanishLine: '1',
     },
     {
       id: 2,
-      englishLine: 'This will be nice',
-      spanishLine: 'Esto será agradable',
+      englishLine: 'I do not know how to imagine you, my love . . . your name',
+      spanishLine: 'No sé cómo imaginarte, mi amor . . . tu nombre alguien lo ',
     },
     {
       id: 3,
-      englishLine: 'This is working',
-      spanishLine: 'Esto está funcionando',
+      englishLine: 'is just a word',
+      spanishLine: 'inventó . . .',
+    },
+    {
+      id: 4,
+      englishLine: 'someone once invented . . . since then no one has been',
+      spanishLine: 'Desde entonces nadie ha podido definir tu rostro y yo',
+    },
+    {
+      id: 5,
+      englishLine: 'able to define',
+      spanishLine: 'desde niño',
+    },
+    {
+      id: 7,
+      englishLine: 'your appearance, and since I was a child I have wanted',
+      spanishLine: 'quise verte',
+    },
+    {
+      id: 8,
+      englishLine: 'to see you',
+      spanishLine: '',
+    },
+    {
+      id: 10,
+      englishLine: '',
+      spanishLine: '',
+    },
+    {
+      id: 11,
+      englishLine: '2',
+      spanishLine: '2',
     },
   ],
 };
