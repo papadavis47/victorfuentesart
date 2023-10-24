61ab22ff8cb190c3d6f62f8ca76a49551785417f
John William Davis <jwdavisdeveloper@gmail.com>
2023-10-23 21:00:46 -0700

    add clsx and tailwind merge, make small addtion to data

--- src/data/poems.ts
+++ src/data/poems.ts
@@ -45,5 +45,95 @@ export const poems = {
       englishLine: '2',
       spanishLine: '2',
     },
+    {
+      id: 1,
+      englishLine: '1',
+      spanishLine: '1',
+    },
+    {
+      id: 2,
+      englishLine: 'I do not know how to imagine you, my love . . . your name',
+      spanishLine: 'No sé cómo imaginarte, mi amor . . . tu nombre alguien lo ',
+    },
+    {
+      id: 3,
+      englishLine: 'is just a word',
+      spanishLine: 'inventó . . .',
+    },
+    {
+      id: 4,
+      englishLine: 'someone once invented . . . since then no one has been',
+      spanishLine: 'Desde entonces nadie ha podido definir tu rostro y yo',
+    },
+    {
+      id: 5,
+      englishLine: 'able to define',
+      spanishLine: 'desde niño',
+    },
+    {
+      id: 7,
+      englishLine: 'your appearance, and since I was a child I have wanted',
+      spanishLine: 'quise verte',
+    },
+    {
+      id: 8,
+      englishLine: 'to see you',
+      spanishLine: '',
+    },
+    {
+      id: 10,
+      englishLine: '',
+      spanishLine: '',
+    },
+    {
+      id: 11,
+      englishLine: '2',
+      spanishLine: '2',
+    },
+    {
+      id: 1,
+      englishLine: '1',
+      spanishLine: '1',
+    },
+    {
+      id: 2,
+      englishLine: 'I do not know how to imagine you, my love . . . your name',
+      spanishLine: 'No sé cómo imaginarte, mi amor . . . tu nombre alguien lo ',
+    },
+    {
+      id: 3,
+      englishLine: 'is just a word',
+      spanishLine: 'inventó . . .',
+    },
+    {
+      id: 4,
+      englishLine: 'someone once invented . . . since then no one has been',
+      spanishLine: 'Desde entonces nadie ha podido definir tu rostro y yo',
+    },
+    {
+      id: 5,
+      englishLine: 'able to define',
+      spanishLine: 'desde niño',
+    },
+    {
+      id: 7,
+      englishLine: 'your appearance, and since I was a child I have wanted',
+      spanishLine: 'quise verte',
+    },
+    {
+      id: 8,
+      englishLine: 'to see you',
+      spanishLine: '',
+    },
+    {
+      id: 10,
+      englishLine: '',
+      spanishLine: '',
+    },
+    {
+      id: 11,
+      englishLine: '2',
+      spanishLine: '2',
+    },
   ],
 };
